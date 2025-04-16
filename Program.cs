eeba3f7906ee724145810ae7cbe34835e37ff2ae
imp4ct <64093562+imp4ctdev@users.noreply.github.com>
2025-04-16 22:19:51 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,15 +1,19 @@
-﻿using System;
+using System;
 using ScottPlot;
 using System.Collections.Generic;
+using ScottPlot.Hatches;
+using System.Reflection.Metadata;
+using ScottPlot.Palettes;
 
 var xs = new List<double>();
 var ys = new List<double>();
 
 
-for(double x=-3; x<=3;x+=0.01 ){
+for(double x=-5; x<=5;x+=0.01){
     xs.Add(x);
     ys.Add(szmierwiastek(x));
 }
+
 
 double szmierwiastek(double x){
     x = Math.Abs(x);
@@ -33,5 +37,12 @@ d.MarkerSize = 0;
 plt.Title("Szmierwiastki");
 plt.XLabel("x");
 plt.YLabel("szm(x)");
+
+var lineY =plt.Add.HorizontalLine(0);
+var lineX =plt.Add.VerticalLine(0);
+
+lineY.LinePattern=LinePattern.Dashed;
+lineX.LinePattern=LinePattern.Dashed;
+
 plt.SaveSvg("Szmierwiastek.svg",1920,1080);
 System.Console.WriteLine("Saved");
